f26b022779c4159621fe6f6779505a7c72c14202
LeViet810 <minhviethoctap@gmail.com>
2020-05-03 15:56:06 +0800

    'done'

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using HtmlAgilityPack;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace WebCrawler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = StartCrawler();
+            foreach (var item in x)
+                Console.WriteLine(item + "\n");
+            
+            Console.ReadLine();
+
+
+        }
+
+        private  static List<Story> StartCrawler()
+        {
+            var html = @"https://www.straitstimes.com";
+            HtmlWeb web = new HtmlWeb();
+            var htmlDoc = web.Load(html);
+
+            var stories = new List<Story>();
+
+            //select only div contains top-stories
+            string sign_top_stories = @"Top Stories";
+            var node_top_stories = htmlDoc.DocumentNode.Descendants("div").Where(node => node.GetAttributeValue("data-vr-zone", "").Contains(sign_top_stories));
+
+            
+            foreach (var item in node_top_stories)
+            {
+                var storieHeadline = item.Descendants("span").Where(node => node.GetAttributeValue("class", "").Equals("story-headline")).FirstOrDefault().Element("a");
+                var story = new Story(html + storieHeadline.GetAttributeValue("href", "null"), storieHeadline.InnerText);
+                stories.Add(story);
+            }
+            return stories;
+        }
+    }
+}
---
+++ Story.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebCrawler
+{
+    class Story
+    {
+        public string link { get; private set; }
+        public string title { get; private set; }
+
+        public Story(string link, string title)
+        {
+            this.link = link;
+            this.title = title;
+        }
+
+        
+        public override string ToString()
+        {
+            return $"link = {link}, title = {title}";
+        }
+    }
+}
